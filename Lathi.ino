e50adc86e48770d60dd98dd6ce3f9a63b36d35b8
name.tar.xz <git.3myp3@slmail.me>
2023-08-01 09:09:27 +0530

    Add distances

--- Lathi.ino
+++ Lathi.ino
@@ -1,47 +1,77 @@
-#include <L298NX2.h>
+w #include<L298NX2.h>
 #include <HCSR04.h>
 
-HCSR04 front(2,3);//trigger , echo |10cm away form wall to consider it as a wall, forward=5
-HCSR04 ultra2(4,5);
-HCSR04 ultra3(6,7);
+    HCSR04 front(9, 10); // trigger , echo |10cm away form wall to consider it
+                         // as a wall, forward=5
+HCSR04 left(11, 12);
+HCSR04 right(3, 4);
 
-int IN1_A = 8;
-int IN2_A = 9;
-int IN1_B = 10;
-int IN2_B = 11;
+int IN1_A = 5;
+int IN2_A = 6;
+int IN1_B = 7;
+int IN2_B = 8;
 
 L298NX2 motor(IN1_A, IN2_A, IN1_B, IN2_B);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  motor.forwardForB(1000);
-  motor.forwardForA(1000);
-
-    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(front.dist());
+  double frontDist = front.dist();
+  double leftDist = left.dist();
+  double rightDist = right.dist();
 
+  delay(2500);
+  if (frontDist > 5) {
+
+    motor.forwardForA(100); // so that dude doesnt crash cause fatness
+    motor.backwardForB(100);
+    forwardForever();
+    if (leftDist <= 10) {
+      // issawall/object idk append array bs
+      Serial.print(1);
+    } else {
+      // issaemptyspace idk append array bs
+      Serial.print(0);
+    }
+  } else if (frontDist < 5) {
+    // end array
+    // start a new array
+    uturn();
+    forwardForever();
+    if (left > 5) {
+      // issaemptyspace idk append array bs
+      Serial.println(0);
+
+    } else {
+      // issa object idk append
+      Serial.print(1);
+    }
+  }
 }
-void rightTurn(){
-  motor.forwardForA(1000);
-  motor.backwardForB(1000);
-}
-void leftTurn(){
-  motor.forwardForA(1000);
-  motor.backwardForB(1000);
-}
-void uTurn(){
-  motor.backwardForA(500);
-  motor.backwardForB(500);
-  motor.stopA();
-  motor.stopB();
-  rightTurn();
+void forwardForever() {
   motor.forwardA();
   motor.forwardB();
+}
+void backwardForever() {
+  motor.backwardA();
+  motor.backwardB();
+}
+void rightTurn() {
+  motor.forwardForA(400);
+  motor.backwardForB(400);
+}
+void leftTurn() {
+  motor.forwardForA(400);
+  motor.backwardForB(400);
+}
+void uTurn() {
+
+  motor.backwardForA(250);
+  motor.backwardForB(250);
+  rightTurn();
   rightTurn();
 }
