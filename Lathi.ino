b185bc42724a7554cc05d0c1e9fcd5513b59387f
name.tar.xz <git.3myp3@slmail.me>
2023-08-01 14:16:30 +0530

    formatting

--- Lathi.ino
+++ Lathi.ino
@@ -1,5 +1,11 @@
 #include <L298NX2.h>
 
+#define usE1 10
+#define usT1 9
+#define usE2 12
+#define usT2 11
+#define usE3 4
+#define usT3 3
 int IN1_A = 5;
 int IN2_A = 6;
 int IN1_B = 7;
@@ -8,65 +14,36 @@ int IN2_B = 8;
 int uTBOB;
 String str;
 
-#define usE1 10
-#define usT1 9
-#define usE2 12
-#define usT2 11
-#define usE3 4
-#define usT3 3
-
 long duration, cm;
 
 L298NX2 motor(IN1_A, IN2_A, IN1_B, IN2_B);
 
-void setup() {
-  Serial.begin(9600);
-
-  pinMode(usE1, INPUT);
-  pinMode(usT1, OUTPUT);
-  pinMode(usE2, INPUT);
-  pinMode(usT2, OUTPUT);
-  pinMode(usE3, INPUT);
-  pinMode(usT3, OUTPUT);
-
-  double frontDist = getDistance(1, "cm");
-  double leftDist = getDistance(2, "cm");
-  double rightDist = getDistance(3, "cm");
-
-  if (leftDist > rightDist) {
-    uTBOB = 1;
-    Serial.println("help");
-  } else if (leftDist == rightDist) {
-    Serial.println("shit");
+long getDistance(int sensor = 1, String type = "cm") {
+  int trig, echo;
+  if (sensor == 1) {
+    trig = usT1;
+    echo = usE1;
+  } else if (sensor == 2) {
+    trig = usT2;
+    echo = usE2;
+  } else if (sensor == 3) {
+    trig = usT3;
+    echo = usE3;
   } else {
-    uTBOB = 0;
-    Serial.println("no help");
+    trig = usT1;
+    echo = usE1;
   }
-  Serial.println("uTBOB:" + String(uTBOB));
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+  cm = (duration / 2) / 29.1;
+  return cm;
 }
-
-void loop() {
-  double frontDist = getDistance(1, "cm");
-  double leftDist = getDistance(2, "cm");
-  double rightDist = getDistance(3, "cm");
-
-  delay(1000);
-
-  if (frontDist <= 5) {
-    motor.stopA();
-    motor.stopB();
-    uTurn(uTBOB);
-    str += "\n";
-  } else {
-    forwardForever();
-    if (leftDist <= 10 || rightDist <= 10) {
-      str += "1";
-    } else {
-      str += "0";
-    }
-  }
-}
-
 void forwardForever() {
   motor.forwardA();
   motor.forwardB();
@@ -98,32 +75,53 @@ void uTurn(int uTBOB) {
     motor.forwardForA(150);
     motor.forwardForB(150);
     leftTurn();
+  }}  
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(usE1, INPUT);
+  pinMode(usT1, OUTPUT);
+  pinMode(usE2, INPUT);
+  pinMode(usT2, OUTPUT);
+  pinMode(usE3, INPUT);
+  pinMode(usT3, OUTPUT);
+
+  double frontDist = getDistance(1, "cm");
+  double leftDist = getDistance(2, "cm");
+  double rightDist = getDistance(3, "cm");
+
+  if (leftDist > rightDist) {
+    uTBOB = 1;
+    Serial.println("help");
+  } else if (leftDist == rightDist) {
+    Serial.println("shit");
+  } else {
+    uTBOB = 0;
+    Serial.println("no help");
   }
+  Serial.println("uTBOB:" + String(uTBOB));
 }
 
-long getDistance(int sensor = 1, String type = "cm") {
-  int trig, echo;
-  if (sensor == 1) {
-    trig = usT1;
-    echo = usE1;
-  } else if (sensor == 2) {
-    trig = usT2;
-    echo = usE2;
-  } else if (sensor == 3) {
-    trig = usT3;
-    echo = usE3;
+void loop() {
+  double frontDist = getDistance(1, "cm");
+  double leftDist = getDistance(2, "cm");
+  double rightDist = getDistance(3, "cm");
+  //Serial.println("frontDist:" + String(frontDist) + "\nleftDist:" +
+  //               String(leftDist) + "\nrightDist" + String(rightDist) + "\n");
+  delay(1000);
+
+  if (frontDist <= 5) {
+    motor.stopA();
+    motor.stopB();
+    uTurn(uTBOB);
+    str += "\n";
   } else {
-    trig = usT1;
-    echo = usE1;
+    forwardForever();
+    if (leftDist <= 10 || rightDist <= 10) {
+      str += "1";
+    } else {
+      str += "0";
+    }
   }
-
-  digitalWrite(trig, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-
-  duration = pulseIn(echo, HIGH);
-  cm = (duration / 2) / 29.1;
-  return (type == "cm") ? cm : inches;
+  Serial.println("---\n"+str+"\n---");
 }
