e2b6002e0f34f807fb1d2ea330b07ce3f7734a22
name.tar.xz <git.3myp3@slmail.me>
2023-08-03 14:46:42 +0530

    most of nodeMCU

--- nodeMCU.ino
+++ nodeMCU.ino
@@ -16,22 +16,19 @@ const char fingerprint[] PROGMEM = "B4 36 2A 5B 51 D2 7C F2 E0 AA B2 55 BF 14 48
 void setup() {
 
     Serial.begin(115200);
-
-    WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+    WiFi.mode(WIFI_OFF);//Prevents reconnection issue (taking too long to connect)
     delay(1000);
-    WiFi.mode(WIFI_STA);        //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
-
-    WiFi.begin(ssid, password);     //Connect to your WiFi router
+    WiFi.mode(WIFI_STA);//Only Station No AP, This line hides the viewing of ESP as wifi hotspot
+    WiFi.begin(ssid, password);//Connect to your WiFi router
     Serial.println("");
-
+    //
     Serial.print("Connecting");
-    // Wait for connection
+// Wait for connection
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-
-    //If connection successful show IP address in serial monitor
+//If connection successful show IP address in serial monitor
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
@@ -41,74 +38,39 @@ void setup() {
 }
 
 void loop() {
-    if (Serial.available()) {
-    Serial.flush();
-    String data=Serial.readString();
-    Serial.flush();
-    Serial.end();   /*end serial communication*/
-    Serial.begin(115200);  /*clear serial buffer*/
-
-    if(data!=""||data!=" "||data!="\n"){
-      //Serial.print(data);
-      WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
-  
-      Serial.print(host);
-    
-      Serial.printf("Using fingerprint '%s'\n", fingerprint);
-      httpsClient.setFingerprint(fingerprint);
-      httpsClient.setTimeout(15000); // 15 Seconds
-      delay(1000);
-      
-      Serial.print("HTTPS Connecting");
-      int r=0; //retry counter
-      while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
-          delay(100);
-          Serial.print(".");
-          r++;
-      }
-      if(r==30) {
-        Serial.println("Connection failed");
-      }
-      else {
-        Serial.println("Connected to web");
-      }
-      
-      String  Link;
-      Link = "/api/setData/"+data;
-      digitalWrite(2,HIGH);
-      delay(500);
-      digitalWrite(2,LOW);
-      delay(500);
-    
-      Serial.print("requesting URL: ");
-      Serial.println(String(host)+Link);
-      
-    
-      httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
-                   "Host: " + host + "\r\n" +               
-                   "Connection: close\r\n\r\n");
-    
-      Serial.println("request sent: "+String(host)+Link);
-                      
-      while (httpsClient.connected()) {
+    WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
+    Serial.println(host);
+    Serial.printf("Using fingerprint '%s'\n", fingerprint);
+    httpsClient.setFingerprint(fingerprint);
+    httpsClient.setTimeout(15000); // 15 Seconds
+    delay(1000);
+    Serial.print("HTTPS Connecting");
+    int r=0; //retry counter
+    while((!httpsClient.connect(host, httpsPort)) && (r < 30)) {
+        delay(100);
+        Serial.print(".");
+        r++;
+    }
+    if(r==30) {
+        Serial.println("\nConnection failed");
+    }
+    else {
+        Serial.println("\nConnected to web");
+    }
+    String Link;
+    Link = "/api/setData/00000000\\n1111111111111111\\n0000000000000000\\n1111111111111111";
+    Serial.print("requesting URL: ");
+    Serial.println(host+Link);
+    httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+                      "Host: " + host + "\r\n" +
+                      "Connection: close\r\n\r\n");
+    Serial.println("request sent");
+    while (httpsClient.connected()) {
         String line = httpsClient.readStringUntil('\n');
         if (line == "\r") {
-          Serial.println("headers received");
-          break;
-        } 
-      }
-    
-      Serial.println("reply was:");
-      Serial.println("==========");
-      String line;
-      while(httpsClient.available()){
-        line = httpsClient.readStringUntil('\n');  //Read Line by Line 
-        Serial.println(line); //Print response
-      }
-      Serial.println("==========");
-      Serial.println("closing connection");
-          
-      delay(2000);  //GET Data at every 2 seconds 2000
+            Serial.println("Headers received");
+            break;
+        }
     }
-  }
+
 }
